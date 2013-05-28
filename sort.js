f0950532d099b22d43c72cb9c4022172767c033c
Paul Miller <paul@paulmillr.com>
2013-05-28 20:13:41 +0300

    Add language field.

--- sort.js
+++ sort.js
@@ -1,14 +1,25 @@
 var path = './repos.json';
 
+// Uncomment when converting from tags:
+// .map(function(item) {
+//   var languages = ["c", "c++", "c#", "coffeescript", "haskell", "java", "javascript", "objective-c", "perl", "php", "python", "ruby", "scala"];
+//   var language = item.tags.filter(function(_) {
+//     return languages.indexOf(_) !== -1;
+//   })[0];
+//   if (language) {
+//     var index = item.tags.indexOf(language);
+//     item.tags.splice(index, 1);
+//     item.language = language;
+//   }
+//   return item;
+// })
+
 var data = require(path)
   .map(function(item) {
-    var closed = item.closed;
-    var merged = item.merged;
-    delete item.closed;
-    delete item.merged;
-    item.closed = closed;
-    item.merged = merged;
-    return item;
+    return {
+      repo: item.repo, language: item.language,
+      closed: item.closed, merged: item.merged, tags: item.tags
+    };
   })
   .sort(function(a, b) {
     if (a.repo < b.repo) return -1;
